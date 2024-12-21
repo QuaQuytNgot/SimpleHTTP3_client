033490a784f151bc1955c270b3bc36a09ffabd18
HoangBui <huyhoang0160072k5@gmail.com>
2024-12-21 23:33:15 +0700

    prj

---
+++ build/CMakeFiles/Run.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Run.dir/run.c.o"
+  "CMakeFiles/Run.dir/run.c.o.d"
+  "Run"
+  "Run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
